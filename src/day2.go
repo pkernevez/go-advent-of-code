41a3c6ff17b7de851136cd78c9e05fea287686be
Philippe Kernevez <pkernevez@kleis.ch>
2023-11-21 22:19:29 +0100

    Day2 part2

--- src/day2.go
+++ src/day2.go
@@ -23,6 +23,17 @@ func data2() []match {
 	}
 	return result
 }
+func dataReco2() []match {
+	lines := ReadLines("day2.txt")
+	var result []match = nil
+	for _, line := range lines {
+		// split line into opponent and me
+		opponent := fromOpponent(line[0:1])
+		me := fromReco(opponent, line[2:3])
+		result = append(result, match{opponent, me})
+	}
+	return result
+}
 
 func Day2() {
 	log.Print("Running day 2")
@@ -32,9 +43,47 @@ func Day2() {
 	for _, fight := range data {
 		globalScore += score(fight)
 	}
-	log.Printf("Global score: %d", globalScore)
-}
+	log.Printf("Global score from command: %d", globalScore)
 
+	data2 := dataReco2()
+	var globalRecoScore int
+	for _, fight := range data2 {
+		globalRecoScore += score(fight)
+	}
+	log.Printf("Global score from reco: %d", globalRecoScore)
+}
+func fromReco(opponent RPS, s string) RPS {
+	switch s {
+	case "X": // Lose
+		switch opponent {
+		case Rock:
+			return Scissors
+		case Paper:
+			return Rock
+		case Scissors:
+			return Paper
+		}
+	case "Y": // Raw
+		switch opponent {
+		case Rock:
+			return Rock
+		case Paper:
+			return Paper
+		case Scissors:
+			return Scissors
+		}
+	case "Z": // Win
+		switch opponent {
+		case Rock:
+			return Paper
+		case Paper:
+			return Scissors
+		case Scissors:
+			return Rock
+		}
+	}
+	panic("Unknown move")
+}
 func fromOpponent(s string) RPS {
 	switch s {
 	case "A":
